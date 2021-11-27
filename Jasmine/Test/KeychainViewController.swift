a263a82a7b337cc74b3ea0f941b2817c66c30bfd
ebamboo <1453810050@qq.com>
2021-11-27 18:31:00 +0800

    key chain 初步引入

---
+++ Jasmine/Test/KeychainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  KeychainViewController.swift
+//  Jasmine
+//
+//  Created by ebamboo on 2021/11/27.
+//
+
+import UIKit
+
+class KeychainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "key chain"
+    }
+
+}
--- Jasmine/ViewController.swift
+++ Jasmine/ViewController.swift
@@ -9,9 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    let titles = ["Keychain"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Jasmine"
     }
     
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        }
+        cell?.textLabel?.text = titles[indexPath.row]
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+        if indexPath.row == 0 {
+            navigationController?.pushViewController(KeychainViewController(), animated: true)
+            return
+        }
+    }
+    
+}

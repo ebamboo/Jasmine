f590c9197576053cda978534ccdda790b0012fac
ebamboo <1453810050@qq.com>
2022-07-03 18:09:56 +0800

    UIImage+Tools 优化完成

--- Jasmine/Source Files/Category/UIImage+Tools.swift
+++ Jasmine/Source Files/Category/UIImage+Tools.swift
@@ -7,34 +7,11 @@
 
 import UIKit
 
-// MARK: - 翻转和旋转
-
-
-// MARK: - 压缩图片
-
 public extension UIImage {
     
-    func compressedImage(size: CGSize) -> UIImage? {
-        if size == self.size {
-            return self
-        }
-        UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
-        draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return newImage;
-    }
-    
-    func compressedImage(scale: CGFloat) -> UIImage? {
-        if scale == 1.0 {
-            return self
-        }
-        let tempSize = CGSize(width: self.size.width * scale, height: self.size.height * scale)
-        return compressedImage(size: tempSize)
-    }
-    
     ///
-    /// 苹果提供的图片压缩处理，极大地降低了内存使用
+    /// 苹果提供的图片压缩法
+    /// Downsampling large images for display at smaller size
     /// https://developer.apple.com/videos/play/wwdc2018/219/?time=26
     ///
     static func downsample(image data: Data, to pointSize: CGSize, scale: CGFloat) -> UIImage {
@@ -61,22 +38,5 @@ public extension UIImage {
         let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampleOptions)!
         return UIImage(cgImage: downsampledImage)
     }
-    
-}
-
-// MARK: - 图片转换
-
-public extension UIImage {
-    
-    ///
-    /// 图片与 base64 互转
-    ///
-    func base64(quality: CGFloat = 1.0) -> String? {
-        return jpegData(compressionQuality: quality)?.base64EncodedString(options: .lineLength64Characters)
-    }
-    
- 
-    
-   
     
 }

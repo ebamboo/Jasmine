a8b3646363f947f27368731487e04585345b4e92
ebamboo <1453810050@qq.com>
2022-07-03 17:47:54 +0800

    UIImage+Transform 功能

--- Jasmine/Source Files/Category/UIImage+Transform.swift
+++ Jasmine/Source Files/Category/UIImage+Transform.swift
@@ -6,3 +6,71 @@
 //
 
 import UIKit
+
+public extension UIImage {
+    
+    enum Transform {
+        // 水平方向翻转（镜像）
+        case horizontalMirrored
+        // 垂直方向翻转（镜像）
+        case verticalMirrored
+        
+        // 本身 -- 返回 self
+        case upRotated
+        // 旋转到底部（顺时针旋转180度）
+        case downRotated
+        // 旋转到左边（顺时针旋转-90度）
+        case leftRotated
+        // 旋转到右边（顺时针旋转90度）
+        case rightRotated
+        
+        // 顺时针旋转任意角度
+        case angleRotated(radian: Double)
+    }
+    
+    func transfering(_ transform: Transform) -> UIImage? {
+        switch transform {
+        case .horizontalMirrored:
+            guard let tempCG = cgImage else { return nil }
+            return UIImage(cgImage: tempCG, scale: scale, orientation: .upMirrored)
+        case .verticalMirrored:
+            guard let tempCG = cgImage else { return nil }
+            return UIImage(cgImage: tempCG, scale: scale, orientation: .downMirrored)
+        case .upRotated:
+            return self
+        case .downRotated:
+            guard let tempCG = cgImage else { return nil }
+            return UIImage(cgImage: tempCG, scale: scale, orientation: .down)
+        case .leftRotated:
+            guard let tempCG = cgImage else { return nil }
+            return UIImage(cgImage: tempCG, scale: scale, orientation: .left)
+        case .rightRotated:
+            guard let tempCG = cgImage else { return nil }
+            return UIImage(cgImage: tempCG, scale: scale, orientation: .right)
+        case .angleRotated(let radian):
+            // 计算旋转之后 rotatedSize
+            let tempView = UIView(frame: CGRect(origin: .zero, size: size))
+            tempView.transform = CGAffineTransform(rotationAngle: radian)
+            let rotatedSize = tempView.frame.size
+            // 根据 rotatedSize 创建位图上下文
+            UIGraphicsBeginImageContextWithOptions(rotatedSize, false, scale)
+            guard let context = UIGraphicsGetCurrentContext() else {
+                UIGraphicsEndImageContext()
+                return nil
+            }
+            // 坐标系先平移操作，再旋转操作
+            context.translateBy(x: rotatedSize.width / 2, y: rotatedSize.height / 2)
+            context.rotate(by: radian)
+            // 绘制位图
+            draw(in: CGRect(x: -size.width / 2, y: -size.height / 2, width: size.width, height: size.height))
+            // 获取图片
+            guard let tempImage = UIGraphicsGetImageFromCurrentImageContext() else {
+                UIGraphicsEndImageContext()
+                return nil
+            }
+            UIGraphicsEndImageContext()
+            return tempImage;
+        }
+    }
+    
+}

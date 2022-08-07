823a83a24bcfd913ccee030e3314d293dff1a544
ebamboo <1453810050@qq.com>
2022-08-07 13:38:54 +0800

    添加 WKUserContentController+Handler

---
+++ Jasmine/Source Files/Category/WKUserContentController+Handler.swift
@@ -0,0 +1,53 @@
+//
+//  WKUserContentController+Handler.swift
+//  Jasmine
+//
+//  Created by ebamboo on 2022/8/7.
+//
+
+import UIKit
+import WebKit
+
+public class ScriptMessageHandlerAddition {
+    
+    weak private (set) var userContentController: WKUserContentController?
+    let name: String
+    
+    init(userContentController: WKUserContentController, name: String) {
+        self.userContentController = userContentController
+        self.name = name
+    }
+    
+    deinit {
+        userContentController?.removeScriptMessageHandler(forName: name)
+    }
+    
+}
+
+public extension WKUserContentController {
+    
+    /// 添加响应 ScriptMessage(name) 的 handler
+    func addScriptMessageHandler(for name: String, _ handler: @escaping (_ userContentController: WKUserContentController, _ message: WKScriptMessage) -> Void) -> ScriptMessageHandlerAddition {
+        add(ScriptMessageHandlerTarget(handler), name: name)
+        return ScriptMessageHandlerAddition(userContentController: self, name: name)
+    }
+    
+}
+
+private extension WKUserContentController {
+    
+    class ScriptMessageHandlerTarget: NSObject, WKScriptMessageHandler {
+        
+        var handler: (_ userContentController: WKUserContentController, _ message: WKScriptMessage) -> Void
+        
+        init(_ handler: @escaping (_ userContentController: WKUserContentController, _ message: WKScriptMessage) -> Void) {
+            self.handler = handler
+        }
+        
+        func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+            handler(userContentController, message)
+        }
+        
+    }
+    
+}

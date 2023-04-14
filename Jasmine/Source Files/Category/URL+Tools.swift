7cdbac12a9dffd5a1d538e80e2c1ecbfa9e339f3
ebamboo <1453810050@qq.com>
2023-04-14 14:39:42 +0800

    添加 ULR 参数获取方法；自定义UILabel 间距

---
+++ Jasmine/Source Files/Category/URL+Tools.swift
@@ -0,0 +1,24 @@
+//
+//  URL+Tools.swift
+//  Jasmine
+//
+//  Created by ebamboo on 2023/4/14.
+//
+
+import Foundation
+
+extension URL {
+    
+    /// 参数中 key 为非空字符串，value 值可以为 ""
+    var param: [String: String] {
+        var dic: [String: String] = [:]
+        query?.components(separatedBy: "&").forEach { item in
+            let temp = item.components(separatedBy: "=")
+            if temp.count == 2, !temp.first!.isEmpty {
+                dic[temp.first!] = temp.last!
+            }
+        }
+        return dic
+    }
+    
+}
---
+++ Jasmine/Source Files/Foundation/SpaceLabel.swift
@@ -0,0 +1,38 @@
+//
+//  SpaceLabel.swift
+//  Jasmine
+//
+//  Created by ebamboo on 2023/4/14.
+//
+
+import UIKit
+
+///
+/// 自由控制 UILabel 字间距和行间距
+/// 默认状态等效为 UILabel
+/// 每次设置 wordSpace 或者 lineSpace 之后赋值字符串给 text 才会生效
+///
+class SpaceLabel: UILabel {
+ 
+    @IBInspectable var wordSpace: CGFloat = 0
+    @IBInspectable var lineSpace: CGFloat = 0
+    
+    override var text: String? {
+        didSet {
+            guard let string = text, !string.isEmpty else { return }
+            let attributedString = NSMutableAttributedString(string: string)
+            attributedString.addAttribute(.kern,
+                                          value: wordSpace,
+                                          range: NSRange(location: 0, length: string.count))
+            attributedString.addAttribute(.paragraphStyle,
+                                          value: {
+                                                    let style = NSMutableParagraphStyle()
+                                                    style.lineSpacing = lineSpace
+                                                    return style
+                                                 }(),
+                                          range: NSRange(location: 0, length: string.count))
+            attributedText = attributedString
+        }
+    }
+    
+}
